3b6f10e1c71b1f2f3deda947a774358f48c7df44
raemartmillare <millare.raemart@auf.edu.ph>
2024-08-16 22:51:08 +0800

    thank-you.php

--- lab2a/thank-you.php
+++ lab2a/thank-you.php
@@ -30,7 +30,7 @@ $_SESSION['sex'] = $sex;
 $_SESSION['program'] = $program;
 $_SESSION['address'] = $address;
 $_SESSION['email'] = $email;
-$_password['password'] = $password;
+$_SESSION['password'] = $password;
 $_SESSION['age'] = $age;
 
 $csvFilePath = '../lab2b/registration.csv';
